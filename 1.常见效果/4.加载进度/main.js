de49db0808f02f650b2ecb29567900a2a4cfadd5
LouisWT <18911761491@163.com>
2018-11-13 18:54:13 +0800

    轻慕课logo转动loading

--- 1.常见效果/4.加载进度/main.js
+++ 1.常见效果/4.加载进度/main.js
@@ -1,6 +1,15 @@
 document.onreadystatechange = function () {
+  // 加载完成后让loading消失
   // if (document.readyState == 'complete') {
   //   const loading = document.getElementsByClassName('loading');
   //   loading[0].style.display = 'none';
   // }
+
+  const content = document.getElementById('logo-loading');
+  let degree = 60;
+  let timer = setInterval(function () {
+    content.style.transform = `rotate(${degree}deg)`;
+    degree += 60;
+    if (degree == 360) degree = 0;
+  }, 1000);
 }
