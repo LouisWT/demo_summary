c05505f1e6876dc06dabaeb187c93afc5a5da638
刘文同 <18911761491@163.com>
2018-12-28 15:56:06 +0800

    对冒泡排序优化后最优时间复杂度为 o(n)

--- 8.数据结构与算法/1.排序/2.冒泡排序.js
+++ 8.数据结构与算法/1.排序/2.冒泡排序.js
@@ -13,11 +13,14 @@
 function bubbleSort(arr) {
   if (!arr) return;
   for (let i = arr.length - 1; i > 0; i--) {
+    let didSwap = false;
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        didSwap = true;
       }
     }
+    if (!didSwap) return;
   }
 }
 
