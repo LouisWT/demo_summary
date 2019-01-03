dbd855c4d6ba3af2e7062eb1001b99ed13449c6e
刘文同 <18911761491@163.com>
2019-01-03 15:12:38 +0800

    async 异步控制

--- 2.JavaScript知识点/3.相关题目/7.异步编程控制/4.async函数实现原理.js
+++ 2.JavaScript知识点/3.相关题目/7.异步编程控制/4.async函数实现原理.js
@@ -1,1 +1,14 @@
+// async 函数是什么？一句话，它就是 Generator 函数的语法糖。
 // async 函数的实现原理，就是将 Generator函数 和自动执行器，包装在一个函数里
+
+async function fn(args) {
+  // ...
+}
+
+// 等同于
+function func(args) {
+  // spawn 是一个类似于 co 库的生成器函数控制器
+  return spawn(function* () {
+    // ...
+  })
+}
