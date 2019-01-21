0ae85fc974b1a7bbe328e3fd29b099d6e885a01a
刘文同 <18911761491@163.com>
2019-01-21 15:14:31 +0800

    数据双向绑定

--- 11.框架相关/1.Vue/1.数据双向绑定/Observer.js
+++ 11.框架相关/1.Vue/1.数据双向绑定/Observer.js
@@ -1,3 +1,42 @@
+// 订阅服务中心
+class Dep {
+  constructor() {
+    this.subs = []
+  }
+
+  addSub(sub) {
+    // sub 是 Watcher 实例
+    this.subs.push(sub)
+  }
+
+  notify() {
+    this.subs.forEach(sub => {
+      sub.update()
+    })
+  } 
+}
+
+// es6 只有静态方法，没有静态属性
+Dep.target = null;
+
+// Watcher 即订阅者
+class Watcher {
+  constructor(obj, key, cb) {
+    Dep.target = this;
+    this.cb = cb;
+    this.obj = obj;
+    this.key = key;
+    // 触发属性的 getter 添加监听
+    this.value = obj[key];
+    Dep.target = null;
+  }
+
+  update() {
+    this.value = this.obj[this.key];
+    this.cb(this.value);
+  }
+}
+
 // 监听数据变化 
 function observe(data) {
   if (!data || typeof data !== 'object') return;
@@ -9,17 +48,22 @@ function observe(data) {
 function defineReative(data, key, val) {
   // 如果值也是对象，监听子属性
   observe(val);
+  let dp = new Dep();
   Object.defineProperty(data, key, {
     enumerable: true,
     configurable: false,
     get: function() {
-      // 添加订阅者
+      // 将 Watcher 添加到订阅
+      if (Dep.target) {
+        dp.addSub(Dep.target);
+      }
       return val;
     },
     set: function(newVal) {
       console.log('监听到数据变化', val, '->',newVal);
       val = newVal;
       // 通知订阅器
+      dp.notify();
     }
   })
 }
@@ -28,21 +72,13 @@ let data = {
   name: 'kind'
 }
 
+function update(value) {
+  console.log('dom change', value);
+}
+
 observe(data);
+// 模拟解析到 {{name}}触发的操作
+new Watcher(data, 'name', update);
 
 data.name = 'here';
 console.log(data.name);
-
-// 订阅者
-function Dep() {
-  this.subs = [];
-}
-
-Dep.prototype = {
-  addSub: function (sub) {
-    this.subs.push(sub);
-  },
-  notify: function () {
-    this.subs.forEach(sub => sub.update());
-  }
-}
--- 11.框架相关/1.Vue/1.数据双向绑定/Proxy版本.js
+++ 11.框架相关/1.Vue/1.数据双向绑定/Proxy版本.js
@@ -1,16 +1,31 @@
-function observe(obj) {
-  if (!obj || typeof obj !== 'object') {
-    return;
-  }
-
-  return new Proxy(obj, {
-    get: function(target, key, receiver) {
-      // 添加订阅
-      return Reflect.get(target, key, receiver);
+let onWatch = (obj, setBind, getLogger) => {
+  let handler = {
+    get(target, property, receiver) {
+      getLogger(target, property);
+      return Reflect.get(target, property, receiver);
     },
-    set: function (target, key, value, receiver) {
-      // 通知订阅者
-      return Reflect.set(target, key, value, receiver);
+    set(target, property, value, receiver) {
+      setBind(value);
+      return Reflect.set(target, property, value, receiver);
     }
-  })
+  }
+  return new Proxy(obj, handler);
 }
+
+let data = {
+  name: 'kind'
+}
+
+let value;
+// 解析到 {{name}} 发生的事情
+let p = onWatch(data,
+  v => {
+    console.log(`bind value to ${v}`);
+    value = v
+  },
+  (target, property) => {
+    console.log(`Get '${property}' = ${target[property]}`);
+  })
+
+p.name = 2;
+console.log(p.name);
